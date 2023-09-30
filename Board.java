48b6e5a971e5c81c9ad8adf01d24875f83558aa7
Adi Duggal <123963842+TheOddAdi8@users.noreply.github.com>
2023-09-30 17:27:46 +0000

    Added rowPos and colPos with user input

--- Board.java
+++ Board.java
@@ -1,11 +1,12 @@
+import java.util.Scanner;
+
 public class Board {
-    
+    Scanner shipBot = new Scanner(System.in);
     int numRows = 10;
     int numCols = 10;
     int[] ships = new int[5]; //{2, 3, 3, 4, 5};
 
     int[][] game = new int[numRows][numCols];
-    
 
     public void printBoard() {
 
@@ -15,7 +16,28 @@ public class Board {
                 System.out.print(game[r][c] + " ");
 
             }
-            System.out.println("");
+            System.out.print("\n");
         }
+        System.out.println("");
+    }
+
+    int rowPos = 0;
+    int colPos = 0;
+
+    public void setPiece() {
+        do {
+            System.out.print("Enter the row number of the head of the 2-unit ship (1-10): ");
+            rowPos = shipBot.nextInt();
+        }
+        while (rowPos < 1 || rowPos > 10);
+        rowPos--;
+
+        do {
+            System.out.print("Enter the column number of the head of the 2-unit ship: ");
+            colPos = shipBot.nextInt();
+        }
+        while (colPos < 1 || colPos > 10);
+        colPos--;
+        
     }
 }
--- Tester.java
+++ Tester.java
@@ -1,6 +1,10 @@
 public class Tester {
+    public static void print(String output) {
+        System.out.print(output);
+    }
     public static void main(String[] args) {
         Board b1 = new Board();
         b1.printBoard();
+        b1.setPiece();
     }
 }
