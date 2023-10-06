96fd436b4da1680e5894cc6358d303f887e08a85
Adi Duggal <123963842+TheOddAdi8@users.noreply.github.com>
2023-10-06 17:06:08 -0400

    Added a few more comments, more needs to be done before demo submission

--- UserBoard.java
+++ UserBoard.java
@@ -1,53 +1,57 @@
 import java.util.Scanner;
 
 public class UserBoard {
-    Scanner shipBot = new Scanner(System.in);
-    int numRows = 10;
-    int numCols = 10;
+    Scanner shipBot = new Scanner(System.in); //Scanner class
 
+    //defining number of rows and columns
+    final int numRows = 10;
+    final int numCols = 10;
+
+    //defining ship lengths
     final int destroyerLength = 2;
     final int cruiserLength = 3;
     final int submarineLength = 3;
     final int battleshipLength = 4;
     final int carrierLength = 5;
 
+    //tried to begin making an array for the start and end positions of each ship to determine whether a ship has been suk
     int[] destroyerStartEnd = new int[4];
     int[] cruiserStartEnd = new int[4];
     int[] submarineStartEnd = new int[4];
     int[] battleshipStartEnd = new int[4];
     int[] carrierStartEnd = new int[4];
 
-    int[] ships = new int[5]; //{2, 3, 3, 4, 5};
+    int[][] game = new int[numRows][numCols]; //Game board
 
-    int[][] game = new int[numRows][numCols];
+    //Game board cheat sheet
 
-    //**************
-    //*  0 = empty *
-    //*  1 = ship  *
-    //*  2 = hit   *
-    //*  3 = miss  *
-    //*  4 = sink? *
-    //**************
+    /***************
+     *  0 = empty *
+     *  1 = ship  *
+     *  2 = hit   *
+     *  3 = miss  *
+     *  4 = sink? *
+     **************/
 
-    public void printBoard() {
+    public void printBoard() { //prints out board in 10x10 shape
 
-        for (int r = 0; r < numRows; r++) {
-            for (int c = 0; c < numCols; c++) {
+        for (int r = 0; r < numRows; r++) { //rows
+            for (int c = 0; c < numCols; c++) { //columns
 
-                System.out.print(game[r][c] + " ");
+                System.out.print(game[r][c] + " "); //space for separate numbers
 
             }
-            System.out.print("\n");
+            System.out.print("\n"); //enter to go to next row
         }
         System.out.println("");
     }
 
+    //defining positions and direction, will later be assigned to a value
     int rowPos = 0;
     int colPos = 0;
     int shipDir = 0;
 
-    public void userSetShipPos(String shipName, int shipLength) {
-        //Destroyer (2-units)
+    public void userSetShipPos(String shipName, int shipLength) { //assigns a spot to the head of the ship
         do {
             System.out.println("");
             System.out.print("Enter the row number (1-10) of the head of the " + shipName + " (" + shipLength + " units): ");
@@ -68,7 +72,7 @@ public class UserBoard {
         
     }
     
-    public void userSetShipDir(String shipName, int shipLength) {
+    public void userSetShipDir(String shipName, int shipLength) {//ask user what direction should the ship face
         do {
             System.out.println("What direction would you like the " + shipName + " to face?");
             System.out.print("1. Up \n2. Down \n3. Left \n4. Right \n5. Go back to setting the ship position\n\n");
@@ -76,27 +80,27 @@ public class UserBoard {
         }
         while (shipDir < 1 || shipDir > 4); 
         
-        int endPos;
-        boolean isShip = false;
+        int endPos; //end position of the ship (rear end of ship)
+        boolean isShip = false; //is there a ship already in that location
 
-        if (shipDir == 1) {
+        if (shipDir == 1) { //checks if ship will be out of bounds
             endPos = rowPos + shipLength;
             if (endPos > numRows) {
                 errorMsg();
                 userSetShipDir(shipName, shipLength);
             }
-            else {
+            else { //checks if ship exists in that spot
                 for (int i = rowPos; i < endPos; i++) {
                     if (game[1][colPos] == 1) {
                         isShip = true;
                     }
                 }
-                if (isShip == false) {
+                if (isShip == false) { //if ship doesn't exist
                     for (int i = rowPos; i < endPos; i++) {
                         game[i][colPos] = 1;
                     }
                 }
-                else {
+                else { //if ship exists
                     errorMsg();
                     userSetShipDir(shipName, shipLength);
                 }
