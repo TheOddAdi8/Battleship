7de29755ad3cfbfa846662960325696aaa179b1d
Adi Duggal <duggaadi25@7hills.org>
2023-10-12 17:24:20 -0400

    removed useless print syntax error

--- Instructions.java
+++ Instructions.java
@@ -20,11 +20,6 @@ public class Instructions {
         System.out.println("10. Have fun!");
         System.out.println("(Press enter to continue)");
         String temp = enterBot.nextLine();
-        if (temp.equalsIgnoreCase("$install virus.exe")) {
-            while (true) {
-                System.out.println("Syntax error, insert \"AssignmentOperator Expression\" to complete assignment");
-            }
-        }
         System.out.println("");
     }
 }
